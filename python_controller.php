73c123909b140865f7c98c6e56aeb7a9d730e2cd
Alex Narvey <anarvey@precursor.ca>
2025-11-19 14:51:41 -0600

    Remove unused methods from python_controller.php
    Removed get_scroll_widget and get_python_instances methods from python_controller.php.

--- python_controller.php
+++ python_controller.php
@@ -77,45 +77,6 @@ class Python_controller extends Module_controller
     jsonView($out);
 }
 
-    public function get_scroll_widget($column)
-    {
-        // Remove non-column name characters
-        $column = preg_replace("/[^A-Za-z0-9_\-]]/", '', $column);
-
-        $sql = "SELECT COUNT(CASE WHEN ".$column." <> '' AND ".$column." IS NOT NULL THEN 1 END) AS count, ".$column." 
-                FROM python
-                LEFT JOIN reportdata USING (serial_number)
-                ".get_machine_group_filter()."
-                AND ".$column." <> '' AND ".$column." IS NOT NULL 
-                GROUP BY ".$column."
-                ORDER BY count DESC";
-
-        $queryobj = new Python_model;
-        jsonView($queryobj->query($sql));
-    }
-    
-     public function get_python_instances()
-     {
-        $sql = "SELECT COUNT(CASE WHEN name <> '' AND name IS NOT NULL THEN 1 END) AS count, name 
-                FROM python
-                LEFT JOIN reportdata USING (serial_number)
-                ".get_machine_group_filter()."
-                GROUP BY name
-                ORDER BY count DESC";
-
-        $out = array();
-        $queryobj = new Python_model;
-        foreach ($queryobj->query($sql) as $obj) {
-            if ("$obj->count" !== "0") {
-                $obj->name = $obj->name ? $obj->name : 'Unknown';
-                $out[] = $obj;
-            }
-        }
-
-        jsonView($out);
-     }
-
-
     public function get_data($serial_number = '')
     {
         jsonView(
