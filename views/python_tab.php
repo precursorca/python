82ab3c7d4deb814adb15e6daa1ad57e9dbd8f5ee
Alex Narvey <anarvey@precursor.ca>
2025-11-22 13:52:29 -0600

    Enhance Python tab table with responsive design
    Updated the Python tab table to include a responsive design and improved data handling.

--- views/python_tab.php
+++ views/python_tab.php
@@ -1,16 +1,36 @@
 <div id="python-tab"></div>
 <h2 data-i18n="python.title"></h2>
 
-<table id="python-tab-table"></table>
+<table id="python-tab-table"
+       class="table table-responsive table-striped table-condensed"
+       style="max-width: 800px;">
+    <tbody></tbody>
+</table>
 
 <script>
 $(document).on('appReady', function(){
+
     $.getJSON(appUrl + '/module/python/get_data/' + serialNumber, function(data){
-        var table = $('#python-tab-table');
-        $.each(data, function(key,val){
-            var th = $('<th>').text(i18n.t('python.column.' + key));
-            var td = $('<td>').text(val);
-            table.append($('<tr>').append(th, td));
+
+        $('#python-cnt').text(data.length);
+        
+        var tbody = $('#python-tab-table tbody');
+        tbody.empty();
+
+        $.each(data, function(index, record){
+
+            $.each(record, function(key, val){
+
+                tbody.append(
+                    $('<tr>').append(
+                        $('<th>').text(i18n.t('python.column.' + key)),
+                        $('<td>').text(val)
+                    )
+                );
+
+            });
+
+
         });
     });
 });
